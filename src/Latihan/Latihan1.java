da05e5d6a616c3691b201405f23931e763fd3e7d
Hp <Hp@LAPTOP-TACEHB1Q>
2018-09-12 13:15:51 +0700

    Tambah Hitung Fibonacci

--- src/Latihan/Latihan1.java
+++ src/Latihan/Latihan1.java
@@ -5,6 +5,9 @@
  */
 package Latihan;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 /**
  *
  * @author Hp
@@ -16,7 +19,38 @@ public class Latihan1 {
         System.out.println("\nHitung Fibonacci");
         System.out.println("1,1,2,3,5,8,13,21, ... dst.\n");
     }
+    public static void main(String[]args){
     String identitas = "Salsabila Fawnia Ramadhani K / X RPL 5 / 40";
-    
+
     tampilJudul(identitas);
+    }
+    private static int tampilInput(){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Bilangan ke-: ");
+        int n = scanner.nextInt();
+        
+        return n;
+    }
+    
+        int n = tampilInput();
+        
+    
+    private static BigInteger fibo(int n){
+        
+        BigInteger[] hasil = new BigInteger[n];
+        
+        hasil[0] = BigInteger.ONE;
+        hasil[1] = BigInteger.ONE;
+        
+        for(int i = 2; i < n; i++){
+            hasil[i] = hasil[i-1].add(hasil[i-2]);
+            
+        }
+        return hasil[n-1];
+    }
+    BigInteger hasil = fibo(n);
+    
 }
+
+
